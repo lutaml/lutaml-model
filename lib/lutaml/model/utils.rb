56111655faf8c5d0c46ea9bbb4a9212814256e53
Ronald Tse <ronald.tse@ribose.com>
2024-08-23 12:31:37 +0800

    chore: utils improvement and add spec

--- lib/lutaml/model/utils.rb
+++ lib/lutaml/model/utils.rb
@@ -3,43 +3,42 @@
 module Lutaml
   module Model
     module Utils
-      # Convert string to camel case
-      def self.camel_case(str)
-        return "" if str.nil? || str.empty?
+      class << self
+        # Convert string to camel case
+        def camel_case(str)
+          return "" if str.nil? || str.empty?
 
-        str[0].upcase + str[1..]
-      end
-
-      # Convert string to class name
-      def self.classify(str)
-        str = str.to_s.delete(".")
-
-        str = str.sub(/^[a-z\d]*/) { |match| camel_case(match) || match }
-
-        str.gsub("::", "/").gsub(%r{(?:_|-|(/))([a-z\d]*)}i) do
-          word = Regexp.last_match(2)
-          substituted = camel_case(word) || word
-          Regexp.last_match(1) ? "::#{substituted}" : substituted
+          str.split("/").map { |part| camelize_part(part) }.join("::")
         end
-      end
 
-      # Convert string to snake case
-      def self.snake_case(str)
-        # XML elements allow periods and hyphens
-        str = str.to_s.tr(".", "_")
-        return str.to_s unless /[A-Z-]|::/.match?(str)
+        # Convert string to class name
+        def classify(str)
+          str = str.to_s.delete(".")
+          str = str.sub(/^[a-z\d]*/) { |match| camel_case(match) || match }
 
-        word = str.to_s.gsub("::", "/")
-        word = word.gsub(/([A-Z]+)(?=[A-Z][a-z])|([a-z\d])(?=[A-Z])/) do
-          "#{Regexp.last_match(1) || Regexp.last_match(2)}_"
+          str.gsub("::", "/").gsub(%r{(?:_|-|(/))([a-z\d]*)}i) do
+            word = Regexp.last_match(2)
+            substituted = camel_case(word) || word
+            Regexp.last_match(1) ? "::#{substituted}" : substituted
+          end
         end
-        word = word.tr("-", "_")
-        word.downcase
-      end
 
-      # Convert word to under score
-      def self.underscore(str)
-        snake_case(str).split("/").last
+        # Convert string to snake case
+        def snake_case(str)
+          str = str.to_s.tr(".", "_")
+          return str unless /[A-Z-]|::/.match?(str)
+
+          str.gsub("::", "/")
+            .gsub(/([A-Z]+)(?=[A-Z][a-z])|([a-z\d])(?=[A-Z])/) { "#{$1 || $2}_" }
+            .tr("-", "_")
+            .downcase
+        end
+
+        private
+
+        def camelize_part(part)
+          part.gsub(/(?:_|-|^)([a-z\d])/i) { $1.upcase }
+        end
       end
     end
   end
