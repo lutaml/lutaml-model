79d00c07bfcbe973eb7ce41470a1c8a496eeea8d
0arm0 <arehman0279@gmail.com>
2025-01-01 13:52:31 +0500

    chore: rubocop fixes

--- lib/lutaml/model/serialize.rb
+++ lib/lutaml/model/serialize.rb
@@ -413,8 +413,7 @@ module Lutaml
           mappings = mappings_for(:xml).mappings
 
           if doc.is_a?(Array)
-            raise "May be `collection: true` is" \
-                  "missing for #{self} in #{options[:caller_class]}"
+            raise "May be `collection: true` is missing for #{self} in #{options[:caller_class]}"
           end
 
           if instance.respond_to?(:ordered=) && doc.is_a?(Lutaml::Model::MappingHash)
@@ -439,7 +438,6 @@ module Lutaml
             raise "Attribute '#{rule.to}' not found in #{self}" unless valid_rule?(rule)
 
             attr = attribute_for_rule(rule)
-
             value = if rule.raw_mapping?
                       doc.node.inner_xml
                     elsif rule.content_mapping?
@@ -452,7 +450,6 @@ module Lutaml
                       defaults_used << rule.to
                       attr&.default || rule.to_value_for(instance)
                     end
-
             value = normalize_xml_value(value, rule, attr, options)
             rule.deserialize(instance, value, attributes, self)
           end
@@ -474,7 +471,7 @@ module Lutaml
             attr = attribute_for_rule(rule)
 
             value = if doc.key?(rule.name.to_s) || doc.key?(rule.name.to_sym)
-              element_found_for[rule.id] = true
+                      element_found_for[rule.id] = true
                       doc[rule.name.to_s] || doc[rule.name.to_sym]
                     else
                       attr&.default
