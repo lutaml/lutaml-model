8a97955535b6927f72f7b586ce69248b59c9691c
suleman-uzair <suleman@workytical.com>
2025-01-07 13:21:21 +0500

    finalized rubocop changes

--- lib/lutaml/model/xml_adapter/builder/oga.rb
+++ lib/lutaml/model/xml_adapter/builder/oga.rb
@@ -77,7 +77,8 @@ module Lutaml
             element_name,
             prefix: (prefix_unset = true
                      nil),
-            attributes: {}
+            attributes: {},
+            &block
           )
             @current_namespace = nil if prefix.nil? && !prefix_unset
             prefixed_name = if prefix
@@ -89,9 +90,7 @@ module Lutaml
                             end
 
             if block_given?
-              element(prefixed_name, attributes) do |element|
-                yield(element)
-              end
+              element(prefixed_name, attributes, &block)
             else
               element(prefixed_name, attributes)
             end
