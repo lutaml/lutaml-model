lutaml/lutaml-serializer

feat: added group, choice and sequence

lib/lutaml/model/error.rb
@@ -17,3 +17,6 @@ require_relative "error/unknown_type_error"
 require_relative "error/multiple_mappings_error"
 require_relative "error/collection_true_missing_error"
 require_relative "error/type/invalid_value_error"
+require_relative "error/incorrect_sequence_error"
+require_relative "error/out_of_range_choice_error"
+require_relative "error/no_root_mapping_error"

lutaml/lutaml-model

lib/lutaml/model/error.rb
@@ -17,3 +17,11 @@ require_relative "error/unknown_type_error"
 require_relative "error/multiple_mappings_error"
 require_relative "error/collection_true_missing_error"
 require_relative "error/type/invalid_value_error"
+require_relative "error/incorrect_sequence_error"
+require_relative "error/choice_upper_bound_error"
+require_relative "error/no_root_mapping_error"
+require_relative "error/import_model_with_root_error"
+require_relative "error/invalid_choice_range_error"
+require_relative "error/unknown_sequence_mapping_error"
+require_relative "error/choice_lower_bound_error"
+require_relative "error/no_root_namespace_error"