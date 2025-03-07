dfdf12e219aded8ade5383c9cb36c67e0722f010
HassanAkbar <hassanakbar2@gmail.com>
2025-03-07 16:43:44 +0500

    fix: hash method for recursive elements

--- spec/lutaml/model/comparable_model_spec.rb
+++ spec/lutaml/model/comparable_model_spec.rb
@@ -73,93 +73,98 @@ RSpec.describe Lutaml::Model::ComparableModel do
     end
 
     context "with deeply nested Serializable objects (CeramicCollection)" do
+      let(:first_collection) do
+        ComparableCeramicCollection.new(
+          name: "Blue Collection",
+          featured_piece: ComparableCeramic.new(
+            type: "Bowl",
+            glaze: ComparableGlaze.new(
+              color: "Blue",
+              temperature: 1200,
+              food_safe: true,
+            ),
+          ),
+        )
+      end
+
+      let(:second_collection) do
+        ComparableCeramicCollection.new(
+          name: "Blue Collection",
+          featured_piece: ComparableCeramic.new(
+            type: "Bowl",
+            glaze: ComparableGlaze.new(
+              color: "Blue",
+              temperature: 1200,
+              food_safe: true,
+            ),
+          ),
+        )
+      end
+
       it "compares equal objects with two levels of nesting" do
         # This test compares CeramicCollection objects that contain Ceramic objects,
         # which in turn contain Glaze objects - a two-level deep nesting
-        glaze1 = ComparableGlaze.new(color: "Blue", temperature: 1200,
-                                     food_safe: true)
-        glaze2 = ComparableGlaze.new(color: "Blue", temperature: 1200,
-                                     food_safe: true)
-        ceramic1 = ComparableCeramic.new(type: "Bowl", glaze: glaze1)
-        ceramic2 = ComparableCeramic.new(type: "Bowl", glaze: glaze2)
-        collection1 = ComparableCeramicCollection.new(name: "Blue Collection",
-                                                      featured_piece: ceramic1)
-        collection2 = ComparableCeramicCollection.new(name: "Blue Collection",
-                                                      featured_piece: ceramic2)
-        expect(collection1).to eq(collection2)
-        expect(collection1.hash).to eq(collection2.hash)
+        expect(first_collection).to eq(second_collection)
       end
 
-      it "compares unequal objects with two levels of nesting" do
-        # This test compares CeramicCollection objects that are different at every level:
-        # the collection name, the ceramic type, and the glaze properties
-        glaze1 = ComparableGlaze.new(color: "Blue", temperature: 1200,
-                                     food_safe: true)
-        glaze2 = ComparableGlaze.new(color: "Red", temperature: 1000,
-                                     food_safe: false)
-        ceramic1 = ComparableCeramic.new(type: "Bowl", glaze: glaze1)
-        ceramic2 = ComparableCeramic.new(type: "Plate", glaze: glaze2)
-        collection1 = ComparableCeramicCollection.new(name: "Blue Collection",
-                                                      featured_piece: ceramic1)
-        collection2 = ComparableCeramicCollection.new(name: "Red Collection",
-                                                      featured_piece: ceramic2)
-        expect(collection1).not_to eq(collection2)
-        expect(collection1.hash).not_to eq(collection2.hash)
+      it "generates same hash for objects with two levels of nesting" do
+        expect(first_collection.hash).to eq(second_collection.hash)
+      end
+
+      context "with deeply nested objects that are not equal" do
+        before do
+          second_collection.name = "Red Collection"
+          second_collection.featured_piece.type = "Plate"
+          second_collection.featured_piece.glaze.color = "Red"
+        end
+
+        it "compares unequal objects with two levels of nesting" do
+          # This test compares CeramicCollection objects that are different at every level:
+          # the collection name, the ceramic type, and the glaze properties
+          expect(first_collection).not_to eq(second_collection)
+        end
+
+        it "generates different hashes for objects with two levels of nesting" do
+          expect(first_collection.hash).not_to eq(second_collection.hash)
+        end
       end
     end
 
     context "with recursive relationships" do
-      it "handles circular references" do
+      let(:first_recursive_node) do
         node1 = RecursiveNode.new(name: "A")
-        node2 = RecursiveNode.new(name: "B")
+        node2 = RecursiveNode.new(name: "B", next_node: node1)
         node1.next_node = node2
-        node2.next_node = node1
-
-        node3 = RecursiveNode.new(name: "A")
-        node4 = RecursiveNode.new(name: "B")
-        node3.next_node = node4
-        node4.next_node = node3
-
-        expect(node1).to eq(node3)
-        expect(node1.hash).to eq(node3.hash)
+        node1
       end
 
-      it "detects differences in recursive structures" do
+      let(:second_recursive_node) do
         node1 = RecursiveNode.new(name: "A")
-        node2 = RecursiveNode.new(name: "B")
+        node2 = RecursiveNode.new(name: "B", next_node: node1)
         node1.next_node = node2
-        node2.next_node = node1
-
-        node3 = RecursiveNode.new(name: "A")
-        node4 = RecursiveNode.new(name: "Different")
-        node3.next_node = node4
-        node4.next_node = node3
-
-        expect(node1).not_to eq(node3)
-        expect(node1.hash).not_to eq(node3.hash)
+        node1
       end
 
-      it "keeps the same hash value repeatedly" do
-        node = RecursiveNode.new(name: "A")
-        hash1 = node.hash
-        hash2 = node.hash
-        expect(hash1).to eq(hash2)
+      describe ".eql?" do
+        it "compares equal objects" do
+          expect(first_recursive_node).to eq(second_recursive_node)
+        end
+
+        it "compares unequal objects" do
+          second_recursive_node.name = "X"
+          expect(first_recursive_node).not_to eq(second_recursive_node)
+        end
       end
 
-      it "changes result upon value change" do
-        node1 = RecursiveNode.new(name: "A")
-        node2 = RecursiveNode.new(name: "B")
-        node1.next_node = node2
-        node2.next_node = node1
+      describe ".hash" do
+        it "returns the same hash for equal objects" do
+          expect(first_recursive_node.hash).to eq(second_recursive_node.hash)
+        end
 
-        node3 = RecursiveNode.new(name: "A")
-        node4 = RecursiveNode.new(name: "B")
-        node3.next_node = node4
-        node4.next_node = node3
-        expect(node1).to eq(node3)
-
-        node4.name = "Different"
-        expect(node1).not_to eq(node3)
+        it "returns different hashes for unequal objects" do
+          second_recursive_node.name = "X"
+          expect(first_recursive_node.hash).not_to eq(second_recursive_node.hash)
+        end
       end
     end
   end
