505cf1cd732d7518c4c6dc6a3f425630bb4d089c
Ronald Tse <ronald.tse@ribose.com>
2025-08-13 15:27:34 +0800

    chore: warn when using tomlib on Windows

--- lib/lutaml/model/toml/tomlib_adapter.rb
+++ lib/lutaml/model/toml/tomlib_adapter.rb
@@ -5,6 +5,17 @@ module Lutaml
   module Model
     module Toml
       class TomlibAdapter < Document
+        # Issue warning for problematic platforms
+        if RUBY_PLATFORM.include?("mingw") && RUBY_VERSION < "3.3"
+          require_relative "../services/logger"
+          Logger.warn(
+            "The Tomlib adapter may cause segmentation faults on Windows " \
+            "with Ruby < 3.3 when parsing invalid TOML. Consider using the " \
+            "TomlRB adapter instead.",
+            __FILE__
+          )
+        end
+
         def self.parse(toml, _options = {})
           Tomlib.load(toml)
         end
