97896d1ca88ec22b30c2a53dd817f1b2e73933f2
HassanAkbar <hassanakbar2@gmail.com>
2025-08-27 12:56:03 +0500

    Removes extra code and specs

--- lib/lutaml/model/services/rule_value_extractor.rb
+++ lib/lutaml/model/services/rule_value_extractor.rb
@@ -17,7 +17,6 @@ module Lutaml
       def call
         rule_names.each do |rule_name|
           value = rule_value_for(rule_name)
-          value = transform_mapped_value(value) if should_transform_value?(value)
           return value if Utils.initialized?(value)
         end
 
@@ -47,36 +46,6 @@ module Lutaml
 
       def fetch_value(name)
         Utils.fetch_str_or_sym(doc, name)
-      end
-
-      def transform_mapped_value(value)
-        value.map do |k, v|
-          if v.is_a?(::Hash)
-            transform_hash_value(v, k, options)
-          else
-            transform_simple_value(k, v, options)
-          end
-        end
-      end
-
-      def transform_hash_value(hash_value, key, options)
-        hash_value.merge(
-          {
-            options[:key_mappings].to_instance.to_s => key,
-          },
-        )
-      end
-
-      def transform_simple_value(key, value, options)
-        {
-          options[:key_mappings].to_instance.to_s => key,
-          options[:value_mappings].as_attribute.to_s => value,
-        }
-      end
-
-      def should_transform_value?(value)
-        (options[:key_mappings] || options[:value_mappings]) &&
-          value.is_a?(::Hash)
       end
 
       def convert_to_format(doc, format)
