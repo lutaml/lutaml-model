8cfdffe6a2b3188a3cce8f7bc0ae109d8d455971
HassanAkbar <hassanakbar2@gmail.com>
2026-04-27 12:38:37 +0500

    use module for class definition in spec

--- spec/lutaml/model/derived_attribute_serialization_spec.rb
+++ spec/lutaml/model/derived_attribute_serialization_spec.rb
@@ -1,29 +1,32 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-class DerivedAttributeParent < Lutaml::Model::Serializable
-  attribute :computed_val, :string, method: :computed_value
 
-  xml do
-    root "parent"
-    map_attribute "computed-val", to: :computed_val, render_default: true
-  end
+module DerivedAttributeSerializationSpec
+  class Parent < Lutaml::Model::Serializable
+    attribute :computed_val, :string, method: :computed_value
 
-  def computed_value
-    "hello"
+    xml do
+      root "parent"
+      map_attribute "computed-val", to: :computed_val, render_default: true
+    end
+
+    def computed_value
+      "hello"
+    end
   end
 end
 
 RSpec.describe "derived attribute serialization after XML deserialization" do
   it "keeps derived attributes in YAML output after XML deserialization" do
-    instance = DerivedAttributeParent.from_xml("<parent/>")
+    instance = DerivedAttributeSerializationSpec::Parent.from_xml("<parent/>")
 
     expect(instance.computed_val).to eq("hello")
     expect(instance.to_yaml).to include("computed_val: hello")
   end
 
   it "keeps derived attributes in key-value output after XML deserialization" do
-    instance = DerivedAttributeParent.from_xml("<parent/>")
+    instance = DerivedAttributeSerializationSpec::Parent.from_xml("<parent/>")
 
     expect(instance.to_json).to include("\"computed_val\":\"hello\"")
   end
