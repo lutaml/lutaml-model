3c9cc2cff2fa272eb4017da683ca78b808a7b975
Ronald Tse <ronald.tse@ribose.com>
2026-05-04 06:39:49 +0800

    fix: replace all anonymous splat forwarding for Ruby 3.0-3.1 compat
    Anonymous splat (*) in method body requires Ruby 3.2+, but
    required_ruby_version is >= 3.0.0. Replace all instances with
    named *args and add rubocop:disable Style/ArgumentsForwarding.
    
    Fixes SyntaxError on Ruby 3.1 in:
    - JSON adapters (Oj, MultiJson)
    - KeyValue adapters
    - Listener#call
    - Services::Base.call

--- lib/lutaml/model/services/base.rb
+++ lib/lutaml/model/services/base.rb
@@ -2,8 +2,10 @@ module Lutaml
   module Model
     module Services
       class Base
-        def self.call(*)
-          new(*).call
+        # rubocop:disable Style/ArgumentsForwarding -- anonymous * requires Ruby 3.2+
+        def self.call(*args)
+          new(*args).call
+          # rubocop:enable Style/ArgumentsForwarding
         end
       end
     end
